df795663d2ae844ca2eaa58e50a5327119b6fa7f
Federico Serron Umpierrez <77068927+federico-serron@users.noreply.github.com>
2025-01-12 01:15:08 +0000

    js modificado, necesito llamar funcion desde html

--- src/app.js
+++ src/app.js
@@ -7,5 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  function generator() {
+    let who = ["The dog", "My grandma", "The mailman", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "my phone", "the car"];
+    let when = [
+      "before the class",
+      "when I was sleeping",
+      "while I was exercising",
+      "during my lunch",
+      "while I was praying"
+    ];
+
+    let rdmWho = who[Math.floor(Math.random() * who.length)];
+    let rdmAction = action[Math.floor(Math.random() * action.length)];
+    let rdmWhat = what[Math.floor(Math.random() * what.length)];
+    let rdmWhen = when[Math.floor(Math.random() * when.length)];
+
+    document
+      .getElementById("excuse")
+      .innerHTML(`${rdmWho} ${rdmAction} ${rdmWhat} ${rdmWhen}`);
+  }
   console.log("Hello Rigo from the console!");
 };
